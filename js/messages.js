be5487585bc5eb56550c174b16966a6bb25a6b82
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-06 11:07:50 +0700

    Update messages.js

--- js/messages.js
+++ js/messages.js
@@ -1,15 +1,26 @@
 var mess = {
     message: function(type, text) {
         if(type != '') {
+            const d = new Date();
+            const Hours = d.getHours();
+            const Minutes = d.getMinutes();
             const MESSAGE = document.createElement('div');
             MESSAGE.id = type;
-            setTimeout(() => {
-                MESSAGE.remove();
-            }, 60000)
+            // setTimeout(() => {
+            //     MESSAGE.remove();
+            // }, 60000)
             if(type == 'right') {
-                MESSAGE.innerHTML = `<span>You: ${text}</span>`;
+                if(Minutes < 10) {
+                    MESSAGE.innerHTML = `<span>You: ${text}</span><span>     <${Hours}:0${Minutes}></span>`;
+                } else {
+                    MESSAGE.innerHTML = `<span>You: ${text}</span><span>     <${Hours}:${Minutes}></span>`;
+                }
             } else {
-                MESSAGE.innerHTML = `<span>Mark: ${text}</span>`;
+                if(Minutes < 10) {
+                    MESSAGE.innerHTML = `<span>Mark: ${text}</span><span>     <${Hours}:0${Minutes}></span>`;
+                } else {
+                    MESSAGE.innerHTML = `<span>Mark: ${text}</span><span>     <${Hours}:${Minutes}></span>`;
+                }
             }
             return message_send_show.appendChild(MESSAGE);
         }
@@ -17,7 +28,7 @@ var mess = {
     messwarn: function(text) {},
 }
 
-mess.message('left', 'Wellcom to chat bots!');
+mess.message('left', 'Welcom to chat bots!');
 setTimeout(() => {
     mess.message('left', "All message [");
 }, 5000)
@@ -31,3 +42,35 @@ setTimeout(() => {
     mess.message('left', "20/10");
     mess.message('left', "version ]");
 }, 5100)
+
+// setInterval(() => {
+    mess.message('left', `Rate stars!<button title="Very bad!" class="stars" id="stars1">⭐</button><button title="bad" class="stars" id="stars2">⭐</button><button title="Normal" class="stars" id="stars3">⭐</button><button title="Good!" class="stars" id="stars4">⭐</button><button title="Very good!" class="stars" id="stars5">⭐</button>`);
+// }, 60000)
+
+var _stars_ = {
+    _1: document.getElementById('stars1'),
+    _2: document.getElementById('stars2'),
+    _3: document.getElementById('stars3'),
+    _4: document.getElementById('stars4'),
+    _5: document.getElementById('stars5'),
+}
+
+_stars_._1.addEventListener('mouseover', () => {
+    mess.message('left', "Me very bad 🥺? <1 stars>");
+})
+
+_stars_._2.addEventListener('click', () => {
+    mess.message('left', "Me bad 😥? <2 stars>");
+})
+
+_stars_._3.addEventListener('click', () => {
+    mess.message('left', "thanks you for rate my app <3 stars>");
+})
+
+_stars_._4.addEventListener('click', () => {
+    mess.message('left', "thanks you for rate my app good😁 <4 stars>");
+})
+
+_stars_._5.addEventListener('click', () => {
+    mess.message('left', "thanks you for rate my app very good😘😘😘 <5 stars>");
+})
