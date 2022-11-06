259c1b8e2278bf1b53ddd1e8850a40e5c9ff1bdb
Huyhihi2010 <89264311+Huyhihi2010@users.noreply.github.com>
2022-11-06 11:08:51 +0700

    Update installtion.js

--- js/installtion.js
+++ js/installtion.js
@@ -3,3 +3,4 @@ const message_send_chat = document.getElementById('message-send-chat');
 const send_chat = document.getElementById('send-chat');
 
 var copyMessageBots = false;
+var Question = false;
